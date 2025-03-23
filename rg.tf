fcd7fe81e7fd2b2772c89c69cfcfea4dbddb7977
Alexandre JULIEN <alexandrejulien@users.noreply.github.com>
2025-03-23 11:01:43 +0100

    WIP

--- rg.tf
+++ rg.tf
@@ -1,4 +1,14 @@
+module "main_tags"{
+    source = "./modules/tags"
+    conventional_tags = {
+        Application = "MyApp"
+        Environment = "Dev"
+    }
+}
+
 resource "azurerm_resource_group" "rg" {
     name = var.resource_group_name
     location = var.location
+
+    tags = module.main_tags.conventional_tags
 }
